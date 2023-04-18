2f2a447abf7d465d4c2d04bb8e4ca197c27713e4
evg-zlg <evg.zalogin@gmail.com>
2023-04-18 11:43:17 +0500

    feat: add distribution section on ton page

---
+++ components/ton-page-components/ton-page-distribution/ton-page-distribution.tsx
@@ -0,0 +1,58 @@
+import Image from 'next/image';
+
+import styles from './ton-page-distribution.module.scss';
+import stylesWrapper from '../../../styles/wrapper.module.scss';
+
+import transactionsIcon from './transactions-icon.svg';
+import accountsIcon from './accounts-icon.svg';
+import validatorsIcon from './validators-icon.svg';
+
+function TonPageDistribution() {
+  return (
+    <section className={styles.distribution}>
+      <div className={stylesWrapper.tonWrapper}>
+        <div className={styles.info}>
+          <h2 className={styles.title}>
+            {`Множество участников.
+              Массовое распространение`}
+          </h2>
+          <p className={styles.description}>
+            {`TON масштабируемый и разделяемый. Гибкая архитектура позволяет ему расти и расти без потери производительности.`}
+          </p>
+        </div>
+
+        <div className={styles.counters}>
+          <div className={styles.item}>
+            <h3 className={styles.itemTitle}>Транзакции</h3>
+            <p className={styles.count}>124,401,889</p>
+            <Image
+              alt="transactions"
+              src={transactionsIcon}
+              className={styles.bgIcon}
+            />
+          </div>
+          <div className={styles.item}>
+            <h3 className={styles.itemTitle}>Участники</h3>
+            <p className={styles.count}>2,507,790</p>
+            <Image
+              alt="accounts"
+              src={accountsIcon}
+              className={styles.bgIcon}
+            />
+          </div>
+          <div className={styles.item}>
+            <h3 className={styles.itemTitle}>Валидаторы</h3>
+            <p className={styles.count}>301</p>
+            <Image
+              alt="validators"
+              src={validatorsIcon}
+              className={styles.bgIcon}
+            />
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+}
+
+export { TonPageDistribution };
--- pages/ton/index.tsx
+++ pages/ton/index.tsx
@@ -1,9 +1,15 @@
+import { Inter } from 'next/font/google'
+
 import { TonPageWelcome } from "../../components/ton-page-components";
+import { TonPageDistribution } from "../../components/ton-page-components/ton-page-distribution/ton-page-distribution";
+
+const inter = Inter({subsets: ['cyrillic']});
 
 export default function TonPage() {
   return (
-    <main>
+    <main className={inter.className}>
       <TonPageWelcome />
+      <TonPageDistribution />
     </main>
   );
 }
