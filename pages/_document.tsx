b69b0cb3a7b5d69ea7e3df1ba5ad2d9756b86d84
Vladimir Anokhin <ano.vladimir@gmail.com>
2023-04-21 15:53:08 +0300

    Link WP global styles externally
    Later we can add a github action to move this CSS file from WordPress installation into NextJS repo automatically

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,9 +1,13 @@
 import { Html, Head, Main, NextScript } from 'next/document'
 
 export default function Document() {
+  const staticUrl = process.env.WORDPRESS_STATIC_URL
+
   return (
     <Html lang="en" >
-      <Head />
+      <Head>
+        <link rel="stylesheet" href={`${staticUrl}/static-global-styles.css`} />
+      </Head>
       <body >
         <Main />
         <NextScript />
